01963828e9e9c5d5a126e8ccdf9e4c60216c5388
Denis Pismenny <denis.pismenny@gmail.com>
2014-07-05 21:26:23 +0300

    fix #2 (js)

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 
+	$(".js-clone-btn").click(function() {   
+	       var $div = $(this).parent().find('.contact-input');
+	       $div.find("input")
+	           .last()
+	           .clone()
+	           .appendTo($div)
+	           .val("")
+	       return false;
+	   });
+
+
 });
