2e564044f42a5050dabe05eac3ea8c0092fdf175
Denis Pismenny <denis.pismenny@gmail.com>
2014-07-05 21:51:14 +0300

    fix #4

--- src/js/common.js
+++ src/js/common.js
@@ -10,5 +10,21 @@ $(document).ready(function(){
 	       return false;
 	   });
 
+	$(".js-login-switcher").change(function(){
+		if($('.js-login-switch-tab1').find('.cfe-radio').hasClass('checked')){
+				$('.js-login-switch-tab1').addClass('is-active');
+				$('.js-login-tab1').addClass('is-active');
+				$('.js-login-switch-tab2').removeClass('is-active');
+				$('.js-login-tab2').removeClass('is-active');
+		}
+		if($('.js-login-switch-tab2').find('.cfe-radio').hasClass('checked')){
+				$('.js-login-switch-tab2').addClass('is-active');
+				$('.js-login-tab2').addClass('is-active');
+				$('.js-login-switch-tab1').removeClass('is-active');
+				$('.js-login-tab1').removeClass('is-active');
+		}
+	});
+
+
 
 });
